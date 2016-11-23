6028f2145b27aed4a50c3aa8f5af71e89ec785ab
Teerapong ladlee <teerapong.l@gbtech.co.th>
2016-11-23 17:14:14 +0700

    Change array declaration

--- fizzbuzz/fib_container.go
+++ fizzbuzz/fib_container.go
@@ -3,8 +3,8 @@ package fizzbuzz
 func FizzbuzzContainer() func([]int) []string {
 	return func(input []int) []string {
 
+		var result []string
 		to_string := Fizzbuzz()
-		result := make([]string, 0)
 
 		for _, num := range input {
 			result = append(result, to_string(num))
