1924b4ce32d53cf517f5fb6f9efd8dc085feac03
Teerapong ladlee <blckpearl.sheeper@gmail.com>
2016-11-25 21:16:56 +0700

    Refactor test

--- fizzbuzz/fib_test.go
+++ fizzbuzz/fib_test.go
@@ -19,31 +19,38 @@ func TestResult(t *testing.T) {
 	result = f.Result(1)
 
 	if result != "1" {
-		t.Errorf("Fizzbuzz(1) should return 1,%q is returned", result)
+		t.Errorf("*F.Result(1) should return 1,%q is returned", result)
 	}
 
 	result = f.Result(3)
 
 	if result != "fizz" {
-		t.Errorf("Fizzbuzz(3) should return fizz, %q is returned", result)
+		t.Errorf("*F.Result(3) should return fizz, %q is returned", result)
 	}
 
 	result = f.Result(5)
 
 	if result != "buzz" {
-		t.Errorf("Fizzbuzz(5) should return buzz, %q is returned", result)
+		t.Errorf("*F.Result(5) should return buzz, %q is returned", result)
 	}
 
 	result = f.Result(15)
 
 	if result != "fizzbuzz" {
-		t.Errorf("Fizzbuzz(15) should return fizzbuzz, %q is returned", result)
+		t.Errorf("*F.Result(15) should return fizzbuzz, %q is returned", result)
 	}
 }
 
 func TestAll(t *testing.T) {
 
 	var f fib.F
+	var result []string
+
+	result = f.All()
+
+	if result != nil {
+		t.Errorf("*F.All() on empty should return nil")
+	}
 
 	f.Push(1)
 	f.Push(2)
@@ -56,15 +63,11 @@ func TestAll(t *testing.T) {
 	f.Push(19)
 	f.Push(20)
 
-	result := f.All()
-
-	if result == nil {
-		t.Errorf("FizzbuzzContainer({...}) should not return nil")
-	}
+	result = f.All()
 
 	expected := []string{"1", "2", "fizz", "4", "buzz", "buzz", "fizz", "fizzbuzz", "19", "buzz"}
 
 	if !reflect.DeepEqual(expected, result) {
-		t.Errorf("FizzbuzzContainer({...}) should return expected array, %q is returned", result)
+		t.Errorf("*F.All() should return expected array, %q is returned", result)
 	}
 }
